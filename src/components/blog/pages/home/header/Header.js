3c092d33d38bc4d9cfa4b83802252ecde69954ec
Oliver Beumkes <ollybeumkes@hotmail.co.uk>
2021-12-31 17:39:24 +0000

    Added Blog Backdrop/Header and Logo

--- src/components/blog/pages/home/header/Header.js
+++ src/components/blog/pages/home/header/Header.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../../../../../assets/images/Motekso - Transparent.png'
+import logo from '../../../../../assets/images/Motekso - Blog Logo 2.png'
+import backdropImage from '../../../../../assets/images/shutterstock_621455918.jpg'
 
 class Header extends Component {
-   
+
     render() {
         return (
             <div class="header">
-                <div class="headerWrapper">
-                    <img src={logo}></img>
+                <div class="logo">
+                    <img src={logo} alt="Motekso Simplified Logo"></img>
                     {/* <span class="text">WYou've found the Blog</span> */}
                 </div>
+                {/* <div class="overlay"> */}
+                <img class="backdrop" src={backdropImage} alt="Backdrop Image" />
+  
+                {/* </div> */}
             </div>
         );
     }
---
+++ src/components/blog/posts/Posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Posts extends Component {
+    render() {
+        return (
+            <div>
+                Posts
+            </div>
+        )
+    }
+}
+
+export default Posts
