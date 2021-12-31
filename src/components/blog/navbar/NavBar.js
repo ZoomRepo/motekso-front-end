191e294c499df5ddf71975e018c0cba1be8b5954
Oliver Beumkes <ollybeumkes@hotmail.co.uk>
2021-12-31 18:34:58 +0000

    Added social media links to buttons

--- src/components/blog/navbar/NavBar.js
+++ src/components/blog/navbar/NavBar.js
@@ -11,9 +11,15 @@ class NavBar extends Component {
         return (
             <div class="navbar">
                 <div class="topLeft">
-                    <i class="topIcon fab fa-twitter-square"></i>
-                    <i class="topIcon fab fa-youtube"></i>
-                    <i class="topIcon fab fa-discord"></i>
+                    <a href="https://twitter.com/MoteksoUK">
+                        <i class="topIcon fab fa-twitter-square"></i>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCgzXxwF_XHaC3F4gbLnTyTA">
+                        <i class="topIcon fab fa-youtube"></i>
+                    </a>
+                    <a href="https://discord.gg/XYGp7rDR">
+                        <i class="topIcon fab fa-discord"></i>
+                    </a>
                 </div>
                 <div class="topCenter">
                     <ul class="navBarLinks">
