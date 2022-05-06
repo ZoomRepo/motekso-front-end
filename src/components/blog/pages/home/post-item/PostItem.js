10fb780a464548a7e6bfae37ec4b9e88800ff73e
zoomrepo <ollybeumkes@hotmail.co.uk>
2022-05-06 01:56:34 +0100

    Date format, font set to basic fira sans removal of login feature

--- src/components/blog/pages/home/post-item/PostItem.js
+++ src/components/blog/pages/home/post-item/PostItem.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 import tempImage from '../../../../../assets/images/who-are-we.jpg'
 import { Link } from "react-router-dom";
 import PostImage from './image/postImage'
+import { format } from 'timeago.js';
+
 
 export class PostItem extends Component {
+    clenseDate = (date) => {
+        var date = new Date(date);
+        return date;
+    }
 
     render() {
         return (
@@ -16,8 +22,10 @@ export class PostItem extends Component {
                     <Link to={`/post/${this.props.Content._id}`} class="link">                    
                         <span class="post-title">{this.props.Content.title}</span>
                     </Link>
-                    <span class="post-date">{this.props.Content.date}</span>
-                    <p class="post-description">{this.props.Content.description}
+                    <span class="post-date">
+                        {format(this.props.Content.date)}
+                        </span>
+                        <p class="post-description">{this.props.Content.description}
                     </p>
                 </div>
             </div>
