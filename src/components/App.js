d5bb2ed77f81c88221049246b8f78650dc5e2ade
zoomrepo <ollybeumkes@hotmail.co.uk>
2022-08-08 18:22:11 +0100

    Start of site reskin, boostrap etc init commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { TwitterTweetEmbed, TwitterTimelineEmbed } from 'react-twitter-embed'
-import '../styles/index.scss'
-import Sidebar from './sidebar/Sidebar'
-import BlogPage from './blog/BlogPage'
-
+import React from 'react';     
+import Home from './simplified-site/Home';
+import './simplified-site/cover.css'
 
 function App() {
   return (
-    <main>
-      {/* TODO: automatically hide sidebar until hover over */}
-      {/* <Sidebar/> */}
-      <BlogPage/>
-    </main>
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <img  class="masthead-brand logo" src={require('../assets/images/simple-transparent.png')} />
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="https://medium.com/@ollie_beumkes">Blog</a>
+            {/* <a class="nav-link" href="#">Patreon</a> */}
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        {/* <h1 class="cover-heading">Motekso</h1> */}
+        <br/>
+        <br/>
+        <br/>
+        <p class="lead">
+          Sharing innovative ideas to help educate, how modern technologies can be used to your advantage.</p>
+          <br/>
+        <p class="lead">
+          <a href="https://medium.com/@ollie_beumkes" class="btn btn-lg btn-secondary">Check out the Blog</a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Follow our <a href="https://twitter.com/MoteksoUK">Twitter</a>.</p>
+        </div>
+      </footer>
+      
+    </div>
   );
 }
 
